06c9446237ad16595adccae1c792b66bbc983ffe
dima74 <dm1998@list.ru>
2014-05-29 20:40:41 +0600

    7

--- Calculator.cs
+++ Calculator.cs
@@ -11,6 +11,7 @@ namespace Calculator
 
 		public const int MAX_PRIORITY = 6;
 
+		// + - * / mod pow
 		public static Operation add = new Operation().addNames("+", "плюс").setPriority(2).setOperation((a, b) => a + b);
 		public static Operation sub = new Operation().addNames("-", "минус").setPriority(2).setOperation((a, b) => a - b);
 		public static Operation multi = new Operation().addNames("*", "умножить").setPriority(1).setOperation((a, b) => a * b);
@@ -18,14 +19,36 @@ namespace Calculator
 		public static Operation mod = new Operation().addNames("%", "mod", "остаток от деления").setPriority(1).setOperation((a, b) => a % b);
 		public static Operation pow = new Operation().addNames("^", "встепени").setPriority(2).setOperation((a, b) => (decimal) Math.Pow((double) a, (double) b));
 
+		// + - log ln abs, round, sign, sqrt, sqr
 		public static Operation addUnary = new Operation().addNames("+").setPriority(0).setOperation((decimal a, decimal b) => +b).setUnary(true);
 		public static Operation subUnary = new Operation().addNames("-").setPriority(0).setOperation((decimal a, decimal b) => -b).setUnary(true);
+		public static Operation log = new Operation().addNames("log", "десятичныйЛогарифм").setOperation((a, b) => (decimal) Math.Log10((double) b)).setUnary(true);
+		public static Operation ln = new Operation().addNames("ln", "натуральныйЛогарифм").setOperation((a, b) => (decimal) Math.Log((double) b)).setUnary(true);
+		public static Operation abs = new Operation().addNames("abs", "модуль").setOperation((a, b) => (decimal) Math.Abs((double) b)).setUnary(true);
+		public static Operation round = new Operation().addNames("round", "округлить").setOperation((a, b) => (decimal) Math.Round(b)).setUnary(true);
+		public static Operation sign = new Operation().addNames("sign", "знак").setOperation((a, b) => (decimal) Math.Sign((double) b)).setUnary(true);
+		public static Operation sqrt = new Operation().addNames("sqrt", "корень").setOperation((a, b) => (decimal) Math.Sqrt((double) b)).setUnary(true);
+		public static Operation sqr = new Operation().addNames("sqr", "квадрат").setOperation((a, b) => b * b).setUnary(true);
+
+		// sin cos tg ctg asin acos atg actg, sinh, cosh, tgh, ctgh
 		public static Operation sin = new Operation().addNames("sin", "синус").setOperation((a, b) => (decimal) Math.Sin((double) b)).setUnary(true);
 		public static Operation cos = new Operation().addNames("cos", "косинус").setOperation((a, b) => (decimal) Math.Cos((double) b)).setUnary(true);
 		public static Operation tg = new Operation().addNames("tg", "тангенс").setOperation((a, b) => (decimal) Math.Tan((double) b)).setUnary(true);
 		public static Operation ctg = new Operation().addNames("ctg", "котангенс").setOperation((a, b) => 1 / ((decimal) Math.Tan((double) b))).setUnary(true);
+		public static Operation aSin = new Operation().addNames("aSin", "арксинус").setOperation((a, b) => (decimal) Math.Asin((double) b)).setUnary(true);
+		public static Operation aCos = new Operation().addNames("aCos", "арккосинус").setOperation((a, b) => (decimal) Math.Acos((double) b)).setUnary(true);
+		public static Operation aTg = new Operation().addNames("aTg", "арктангенс").setOperation((a, b) => (decimal) Math.Atan((double) b)).setUnary(true);
+		public static Operation aCtg = new Operation().addNames("aCtg", "арккотангенс").setOperation((a, b) => 1 / ((decimal) (Math.PI / 2 - Math.Atan((double) b)))).setUnary(true);
+		public static Operation sinh = new Operation().addNames("sinh", "гиперболический синус").setOperation((a, b) => (decimal) Math.Sinh((double) b)).setUnary(true);
+		public static Operation cosh = new Operation().addNames("cosh", "гиперболический косинус").setOperation((a, b) => (decimal) Math.Cosh((double) b)).setUnary(true);
+		public static Operation tgh = new Operation().addNames("tgh", "гиперболический тангенс").setOperation((a, b) => (decimal) Math.Tanh((double) b)).setUnary(true);
+		public static Operation ctgh = new Operation().addNames("ctgh", "гиперболический котангенс").setOperation((a, b) => 1 / ((decimal) Math.Tanh((double) b))).setUnary(true);
 
-		public Operation[] operations = { add, sub, multi, div, mod, pow, addUnary, subUnary, sin, cos, tg, ctg };
+		public Operation[] operations = {
+											add, sub, multi, div, mod, pow, 
+											addUnary, subUnary, log, ln, abs, round, sign, sqrt, sqr, 
+											sin, cos, tg, ctg, aSin, aCos, aTg, aCtg, sinh, cosh, tgh, ctgh
+										};
 
 		enum TypePart
 		{
@@ -37,7 +60,7 @@ namespace Calculator
 
 		public Calculator()
 		{
-
+			Math.Round(5.0);
 		}
 
 		public decimal calculate(string question)
@@ -130,6 +153,7 @@ namespace Calculator
 						operation = getFromAllOperations(op => op.names.Contains(partsString[i]) && !op.isUnary);
 						priorities[operation.priority].Add(i);
 					}
+					// test
 					//priorities[operation.priority].Add(i);
 				}
 				order.Add(i);
